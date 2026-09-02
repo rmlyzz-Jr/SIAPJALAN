5468103dd8db76c116f783139f2929d4233b7b4a
rmlyzz-Jr <sistemrpu@gmail.com>
2026-09-02 07:12:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let grafikChart = null;
 // SPREADSHEET CONFIGURATION (Google Apps Script Backend URL)
 // ============================================================
 // ⚠️ GANTI DENGAN URL WEB APP GOOGLE APPS SCRIPT ANDA
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyJv4tuZTeq3ipGjNxYv6Fg3p9ZP53h3vLWajs7w4ajB3-Hj26Bhmc4YtEdss2khkzW/exec'; 
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzQKQcPPBoCLPlzUF9ySr0kYCa0RGoB6Z04uAzTRLNGZcdXu2b6KSqrmEvCrqElEAzSGA/exec'; 
 
 // ============================================================
 // HELPER FUNCTIONS
